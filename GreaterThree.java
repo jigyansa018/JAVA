215101e0af61e7c32cfc917d76aa96e64729fb40
Jigyansa <71638450+jigyansa018@users.noreply.github.com>
2024-10-01 18:31:15 +0530

    Add files via upload

---
+++ GreaterThree.java
@@ -0,0 +1,16 @@
+public class GreaterThree {
+    public static void main(String args[]) {
+        int a = 20, b = 10,c =8;
+        if(a>b &&  a>c) {
+            System.out.println("Greater number is " + a);
+}
+else if(b>a && b>c){
+    System.out.println("Greater number is " + b);
+}
+else
+{
+    System.out.println("Greater number is  " + c);
+
+}
+    }
+}
